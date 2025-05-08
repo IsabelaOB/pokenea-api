cdf946e0de3716dcab08f84286ac35a425a55d67
Nicolas Peña <nicolaspj999@gmail.com>
2025-05-07 20:20:18 -0500

    nueva estructura

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const infoRoute = require('./routes/info');
-const fraseRoute = require('./routes/frase');
+
+const infoRoute = require('./src/routes/info');
+const fraseRoute = require('./src/routes/frase');
+
+app.get('/', (req,res) => {
+  res.send("Bienvenido a la API de Pokeneas.")
+});
 
 app.use('/info', infoRoute);
 app.use('/frase', fraseRoute);
 
 app.listen(port, () => {
-  console.log(`Pokenea API running on port ${port}`);
+  console.log(`Pokenea API running on http://localhost:3000`);
 });
--- src/routes/info.js
+++ src/routes/info.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pokeneas = require('../pokeneas/data');
+const pokeneas = require('../models/pokeneas');
 const os = require('os');
 
 router.get('/', (req, res) => {
